45ddd48c6b1dc90c3ab1cb23638b53122d8ab20f
Rahman Elsirafy <rahman2408@gmail.com>
2021-10-04 04:53:05 -0400

    successfully renders each individual item on page, including the image listed under an item's imageUrl attribute

--- src/Item.js
+++ src/Item.js
@@ -8,6 +8,18 @@ class Item {
       console.log(this)
     }
 
+    renderItem = () => {
+      const {name, description, price, forSale, imageUrl} = this.itemInfo
+      document.querySelector(".item-container").innerHTML += 
+      `<div class="item-card">
+        
+        <h4 class="title">${name}</h4>
+        <p class="description">${description}</p>
+        <p class="price">$${price}</p>
+        <p class="itemPic"><img src=${imageUrl} alt=${name}/></p> 
+      </div>`
+    }
+
     static add(item){
       new Item(item)
     }
@@ -16,6 +28,7 @@ class Item {
       const itemContainer = document.createElement("div")
       itemContainer.classList.add("item-container")
       document.getElementById("main").appendChild(itemContainer)
+      this.all.forEach(item => item.renderItem())
     }
 
 
